3a80a9712465346e24b790c43e55642ec4ef33d8
kiki <at77dot@163.com>
2019-05-03 15:07:52 +0800

    fix problem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -32,6 +32,7 @@ export default {
   },
   methods: {
     handleScroll () {
+      // console.log("scroll")
       const top = document.documentElement.scrollTop
       if (top > 60) {
         let opacity = top / 140
@@ -44,7 +45,11 @@ export default {
     }
   },
   activated () {
+    // 全局事件绑定
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
